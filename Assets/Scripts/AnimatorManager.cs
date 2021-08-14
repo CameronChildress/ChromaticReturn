f61252fcef771d671cc7f8819a6a38c57045316d
CameronChildress <cameronchildress4@gmail.com>
2021-08-14 16:27:56 -0600

    Walking Done, Need to do Camera

---
+++ Assets/Scripts/AnimatorManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    Animator animator;
+    int horizontal;
+    int vertical;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        horizontal = Animator.StringToHash("Horizontal");
+        vertical = Animator.StringToHash("Vertical");
+    }
+
+    public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
+    {
+            //Snapped Movement is a preference
+            //This isnt needed, in .SetFloat use horizontalMovement & verticalMovement
+        //Animation Snapping
+        float snappedHorizontal;
+        float snappedVertical;
+
+        #region SnappedHorizontal Checks
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            snappedHorizontal = 0.5f;
+        }
+        else if (horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = 1f;
+        }
+        else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            snappedHorizontal = -0.55f;
+        }
+        else if (horizontalMovement < -0.55f)
+        {
+            snappedHorizontal = -1f;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+
+        #region SnappedVertical Checks
+        if (verticalMovement > 0 && verticalMovement < 0.55f)
+        {
+            snappedVertical = 0.5f;
+        }
+        else if (verticalMovement > 0.55f)
+        {
+            snappedVertical = 1f;
+        }
+        else if (verticalMovement < 0 && verticalMovement > -0.55f)
+        {
+            snappedVertical = -0.55f;
+        }
+        else if (verticalMovement < -0.55f)
+        {
+            snappedVertical = -1f;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+        //damp time or blend time
+        animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
+        //animator.SetFloat(horizontal, horizontalMovement, 0.1f, Time.deltaTime);
+        //animator.SetFloat(vertical, verticalMovement, 0.1f, Time.deltaTime);
+    }
+}
