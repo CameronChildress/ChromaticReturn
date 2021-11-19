d9c61e46aa9a9b4cce424130006430def392dbc1
CameronChildress <cameronchildress4@gmail.com>
2021-11-18 18:39:49 -0700

    First Build

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
-using UnityEditor.Rendering.PostProcessing;
 using UnityEngine.AI;
 
 public class EnemyManager : CharacterManager
