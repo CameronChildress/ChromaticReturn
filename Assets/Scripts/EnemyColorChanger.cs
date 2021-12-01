f917f736037eee33d76f6ab48e573004183219cf
CameronChildress <cameronchildress4@gmail.com>
2021-12-01 16:55:45 -0700

    Certain Enemies Change the World Color

--- Assets/Scripts/EnemyColorChanger.cs
+++ Assets/Scripts/EnemyColorChanger.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class EnemyColorChanger : MonoBehaviour
 {
+    public bool colorHasChanged = false;
+
+    private void Start()
+    {
+        
+    }
+
     void Update()
+    {
+        if (this.GetComponent<EnemyStats>().currentHealth <= 0 && colorHasChanged == false)
+        {
+            WorldColorManager.Instance.OnChangeWorldProfile();
+            colorHasChanged = true;
+        }
+        else if (this.GetComponent<EnemyStats>().currentHealth > 0 && colorHasChanged == true)
+        {
+            colorHasChanged = false;
+        }
+    }
+
+    public void ChangeColor()
     {
         if (this.GetComponent<EnemyStats>().currentHealth <= 0)
         {
